610c91311771bc0350d61f04bba076828cb4d7a8
Aleksey <p0inter88@mail.ru>
2015-05-13 20:50:52 +0400

    Max number version 2

--- Voronin/src/lab9/Lab9.java
+++ Voronin/src/lab9/Lab9.java
@@ -1,20 +1,21 @@
 package lab9;
 
 import static java.lang.Math.random;
+import static java.lang.Math.max;
 
 //имеется последовательность случайных чисел, найти и вывести наибольшее из них
 public class Lab9 {
     public static void main(String[] args) {
-        int number = (int) (random() * 20);
-        int number2 = (int) (random() * 20);
-        int res = 0;
-        while (number != 0) {
-            if (number > number2) {
-                res = number;
-            }
-            number = (int) (random() * 20);
-            number2 = (int) (random() * 20);
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (random() * 20);
         }
-        System.out.println("Наибольшее число " + res);
+        int maxNumber = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > maxNumber) {
+                maxNumber = max(maxNumber, numbers[i]);
+            }
+        }
+        System.out.println("Наибольшее число " + maxNumber);
     }
 }
