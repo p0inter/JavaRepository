7b58cd665b2fe980dd259fe35aee75363024bf51
Aleksey <p0inter88@mail.ru>
2015-06-03 23:13:04 +0300

    Invert number

--- Voronin/src/lab6/Lab6.java
+++ Voronin/src/lab6/Lab6.java
@@ -4,9 +4,13 @@ package lab6;
 public class Lab6 {
     public static void main(String[] args) {
         int number = 235731;
+        int invertNumber = 0;
+        System.out.println(number);
         while (number != 0) {
-            System.out.print(number % 10);
+            invertNumber *= 10;
+            invertNumber += number % 10;
             number /= 10;
         }
+        System.out.println(invertNumber);
     }
 }
