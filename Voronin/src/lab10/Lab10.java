590bfefc89095c59f6dfa4a1fe93e3183e091f67
Aleksey <p0inter88@mail.ru>
2015-06-03 22:42:10 +0300

    является ли число простым

--- Voronin/src/lab10/Lab10.java
+++ Voronin/src/lab10/Lab10.java
@@ -3,17 +3,17 @@ package lab10;
 //является ли число простым
 public class Lab10 {
     public static void main(String[] args) {
-        int number = 13;
-        for (int i = 2; i < number; i++) {
-            if ((number == 1) || (number == 2) || (number == 3)) {
-                System.out.println(number + " является простым числом");
-            } else if (number % i == 0) {
-                System.out.println(number + " не является простым числом");
-                break;
-            } else {
-                System.out.println(number + " является простым числом");
-                break;
+        boolean isPrime = true;
+        int number = 5;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                isPrime = false;
             }
+        }
+        if(isPrime) {
+            System.out.println(number + " является простым числом");
+        } else {
+            System.out.println(number + " не является простым числом");
         }
     }
 }
